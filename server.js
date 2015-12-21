a943bee495d19049366df5964e9f1324a95ec57b
pangyuanxing <308106832@qq.com>
2015-12-21 10:47:51 +0800

    提交

--- server.js
+++ server.js
@@ -42,8 +42,7 @@ var rootPath = process.cwd(),
         "Origin": "https://www.jikexueyuan.com",
         "Referer" : "https://www.jikexueyuan.com",
         "User-Agent":"Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/31.0.1650.63 Safari/537.36",
-        //"Cookie": querystring.stringify(cookie, '; ', '='),
-        "Cookie": "stat_uuid=1448431777546632295352; stat_fromWebUrl=; stat_ssid=1451048165810; bannerswitch=close; uname=jike_1325794; uid=4191045; code=QI5FCX; authcode=d0b0HUKFYeAZ6EWVOfOqz9ie1gbas%2BMher0WP8kj3iT%2BCDkJa12DNjagnE5CvSUVhJzU6DsBOjTvxUUdTsosjOkkdrxPdrzZQgjbthE%2FT0fRnG8r5Iz0QJMVjpxdBkoP; level_id=3; is_expire=0; domain=7912760454; QINGCLOUDELB=84b10773c6746376c2c7ad1fac354ddfd562b81daa2a899c46d3a1e304c7eb2b|Vm/MZ|Vm/Ka; Hm_lvt_f3c68d41bda15331608595c98e9c3915=1449046287,1449733790,1449819994,1450166887; Hm_lpvt_f3c68d41bda15331608595c98e9c3915=1450167394; _ga=GA1.2.661554090.1447228532; undefined=; stat_isNew=0; MECHAT_LVTime=1450167394734; MECHAT_CKID=cookieVal=006600144533895193666809; connect.sid=s%3A54pbTf550mGbN1R6K7tTpwLZ4FT8AU-u.D3H724SYK%2FnykejuSftJ3zrF%2BYp%2FSjHi2Qur4pdCTaA"
+        "Cookie": "stat_uuid=1448431777546632295352; stat_fromWebUrl=; stat_ssid=1451650049855; _gat=1; uname=jike_1325794; uid=4191045; code=QI5FCX; authcode=95d6Q3NyuxgRTFmcd7B5x%2BM2TOYgZtka1PSAHsMI7krEG2ameV5BSfRXI0hafrSYSHMXS4LCdgt03ccwRkNycKJj2qD%2BhAjA8GPyTiMwPHqVdDSCTeAQ0SjROQmpP5OG; level_id=3; is_expire=0; domain=7912760454; bannerswitch=close; QINGCLOUDELB=1bb571da02dcd2ad08ff904df3dac9a3d562b81daa2a899c46d3a1e304c7eb2b|Vndit|Vndir; XSRF-TOKEN=eyJpdiI6InVDbTNaNlBEanV6VytJY3Y0ZEJiU0E9PSIsInZhbHVlIjoiSW9RWVlvMFdzZHN4TE1oN21aYUJZWHdYWTB4KzBLQzFCXC9vVXFub2FRVzRYeWJmb1BmWEt0M1BJMEFrVVhoeHB1QmlmcDk4bUdMVHFldVg4TUpzalVRPT0iLCJtYWMiOiJmNDI4NjViYWJiODI4ZGI2OWE0Mjc5MGQ4NGQwOTkyM2EyNTM1MzFkMDhjNjljNGRhYTA2MmM2ZDhiN2RiZjRjIn0%3D; laravel_session=eyJpdiI6IkN3TVlqZzZyZlNIdnZFcTlTTGJSXC9BPT0iLCJ2YWx1ZSI6Ikc5ZU5vTGc0V3BoQ0QzRUgwYTREQkcyK0c1OGZHRTJ2azFGenZtWE8xaGxLT3U2MVI0UEtqeVV1RkhZeFE3XC9NVUhjR0k2c0tkYit1aFpmN1wvSUV2dkE9PSIsIm1hYyI6IjBkM2JkMWQ5NjI3ZTFiYTM5ZTYwMGRlMWEwNjRmYTNlNjUyYTczZmEyZTVmNDljNzlmMGE1NzgzNGVkNTljNmYifQ%3D%3D; Hm_lvt_532e28cc3b6b596b381c569d4e6cd0e4=1450166914,1450167116,1450167363,1450664620; Hm_lpvt_532e28cc3b6b596b381c569d4e6cd0e4=1450664627; Hm_lvt_f3c68d41bda15331608595c98e9c3915=1449733790,1449819994,1450166887,1450664535; Hm_lpvt_f3c68d41bda15331608595c98e9c3915=1450664637; _ga=GA1.2.661554090.1447228532; undefined=; stat_isNew=0; MECHAT_LVTime=1450664637320; MECHAT_CKID=cookieVal=006600144533895193666809"
     },
 
     downloadRes = function(course_id, path, cb){
@@ -135,7 +134,7 @@ var rootPath = process.cwd(),
                 var $ = cheerio.load(data),
                 title = $('h1').text().replace(/[^\u4e00-\u9fa5\w-\s]/g, "-"),
                 videolist = $('.video-list h2 a'),
-                learnPath = path + '/' + title,
+                learnPath = path + '/' + learn_uuids[learn_index] + '-' + title,
                 videoLength = videolist.length,
                 videoIndex = 0,
                 filename,
